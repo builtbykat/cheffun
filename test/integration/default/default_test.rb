ec2be52d55b2f6b643fddcff57fd888c26250233
Kat S <enirehtaks@gmail.com>
2021-02-09 15:38:39 -0700

    Add tests
    To check that pkgs installed,
    contents of vhost conf and index.php,
    and http responses

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -3,14 +3,33 @@
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+packages = ['apache2', 'php-mysql', 'php']
+
+packages.each do |pkg|
+	describe package(pkg) do
+		it { should be_installed }
+	end
 end
 
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+describe file('/etc/apache2/sites-available/000-default.conf') do
+	it { should exist }
+	its('owner') { should eq 'www-data' }
+	its('group') { should eq 'www-data' }
+	its('mode') { should cmp '0644' }
+	its(:content) { should include 'DirectoryIndex index.php' }
+	its(:content) { should include 'AllowOverride All' }
+	its(:content) { should include 'Require all granted' }
+end
+
+describe file('/var/www/sandbox/index.php') do
+	it { should exist }
+	its('owner') { should eq 'www-data' }
+	its('group') { should eq 'www-data' }
+	its('mode') { should cmp '0644' }
+	its(:content) { should include 'The time where Josh lives: ' }
+end
+
+describe http('http://localhost') do
+	its('status') { should cmp 200 }
+	its('body') { should include 'The time where Josh lives: ' }
 end
